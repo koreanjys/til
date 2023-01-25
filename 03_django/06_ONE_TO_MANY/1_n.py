bd170eb50572099a040711fef170e5aa66505c09
koreanjys <connecttojys@gmail.com>
2023-01-25 18:03:12 +0900

    start data

--- 03_django/06_ONE_TO_MANY/1_n.py
+++ 03_django/06_ONE_TO_MANY/1_n.py
@@ -4,9 +4,9 @@ a.content = 'qwer'
 a.save()
 
 c1 = Comment()
-c.content = '1234'
-c.article = a
-c.save()
+c1.content = '1234'
+c1.article = a
+c1.save()
 
 c2 = Comment.objects.create(content='4567', article=a)
 
---
+++ 04_data/data.md
@@ -0,0 +1,8 @@
+# 데이터
+
+## 데이터의 수행
+1. Select or Collect (자료수집)
+2. Preprocessing (전처리)
+3. Transform (변환)
+4. Model (모델)
+5. Evaluation (평가)
