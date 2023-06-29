24f5d6fa0bb16e9b1280a746ac3e6081013d7adf
koreanjys <connecttojys@gmail.com>
2023-06-29 17:41:17 +0900

    comp

--- algorism/[2022 기업멤버십 SW캠프_1기] 코딩테스트 특강 코드 모음/hotel_room.py
+++ algorism/[2022 기업멤버십 SW캠프_1기] 코딩테스트 특강 코드 모음/hotel_room.py
@@ -1,42 +1,20 @@
-def solution(queries):
-    
-    
-    # 마지막 n세대
-    n = sorted(queries)[-1][0]
-    
-    # 리스트
-    p_list = ['Rr' for _ in range(n)]
-
-    # 세대 증식
-    for i in range(1, n):
-        # 1세대부터 n세대 까지 (index_0 은 1세대)
-        
-        # 홀수 세대는 Rr 짝수 세대는 Tt
-        # i는 인덱스라 0부터 시작, 고로 i 짝수가 곧 홀수
-        if i % 2 == 1:  # 짝수세대
-            p_list[i] = p_list[i-1].replace('RR', 'TT'*4).replace('Rr', 'TTTtTttt').replace('rr', 'tt'*4)
-        else:
-            p_list[i] = p_list[i-1].replace('TT', 'RR'*4).replace('Tt', 'RRRrRrrr').replace('tt', 'rr'*4)
-    
+def solution(k, room_number):
     answer = []
-    for n, p in queries:
-        if n == 1:
-            char = 'Rr'
-        else:
-            char = p_list[n-1][p*2-2:p*2]
-            char = char.replace('T', 'R').replace('t', 'r')
-        answer.append(char)
-    return answer
 
-print(solution([[16, 2]]))
-        
-                    
-                    
-                    
+    next = {}
+    for num in room_number:
+        _num = num
+        visit = [_num]
+        while _num in next:
+            _num = next[_num]
+            visit.append(_num)
+        answer.append(_num)
                 
+        next_num = _num + 1
+        while next_num in next:
+            next_num += 1
         
-        
-        
-        
-        
-        
+        for num in visit:
+            next[num] = next_num
+    
+    return answer
